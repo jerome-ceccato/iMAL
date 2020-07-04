483bd91f7ac757839860debf437b2bd53ffd56eb
Jérôme <jerome.ceccato@gmail.com>
2020-07-05 01:13:28 +0200

    Added dummies for missing files and updated Pods

---
+++ iMAL/Sources/AppConstants.swift
@@ -0,0 +1,19 @@
+//
+//  AppConstants.swift
+//  iMAL
+//
+//  Created by Jerome Ceccato on 03/06/2019.
+//  Copyright © 2019 IATGOF. All rights reserved.
+//
+
+import Foundation
+
+struct Global {
+    static let privateAPIURL = "https://"
+    static let customAPIURL = "https://"
+    static let contactEmailAddress = "x@gmail.com"
+    static let trelloBoardURL = "https://trello.com/"
+    static let discordURL = "https://discord.gg/"
+    static let twitterConsumerKey = "-"
+    static let twitterConsumerSecret = "-"
+}
